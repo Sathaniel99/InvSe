3bba1ea5f0da6678a4dd32b7cc59c63a69c7c2b4
Adán Luis Quincoces Escalona <adanlqe@gmail.com>
2025-05-23 23:21:19 -0400

    Updated

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from SECRET_KEY import KEY_PROJECT_INVSE
+from .SECRET_KEY import KEY_PROJECT_INVSE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
